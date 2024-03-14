0fea05c8fc52df37dbbffba168aed451e9ab5935
zhangzheyong <zhangzheyong@turingq.com>
2024-03-14 09:47:15 +0800

    quotation marks around word

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Execute python program
 vim.keymap.set("n", "<F3>", "<esc>:w | !python %<cr>", { desc = "Execute current Python file" })
+-- Wrap word with quotes
 vim.keymap.set("n", '<leader>r"', 'viw<esc>a"<esc>hbi"<esc>lel', { desc = 'Wrap word with ""' })
 vim.keymap.set("n", "<leader>r'", "viw<esc>a'<esc>hbi'<esc>lel", { desc = "Wrap word with ''" })
