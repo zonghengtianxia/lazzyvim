4f8aaf1504e9971bfe60ac10433ce76972fa6237
zhangzheyong <zhangzheyong@turingq.com>
2024-04-15 13:21:53 +0800

    codeium

--- lua/plugins/portal.lua
+++ lua/plugins/portal.lua
@@ -5,6 +5,7 @@ return {
     "cbochs/grapple.nvim",
     "ThePrimeagen/harpoon",
   },
+
   config = function()
     vim.keymap.set("n", "<leader>o", "<cmd>Portal jumplist backward<cr>")
     vim.keymap.set("n", "<leader>i", "<cmd>Portal jumplist forward<cr>")
